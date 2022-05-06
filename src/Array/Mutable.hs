7a9c864ad92150bef20ba1512b712da9faef07e0
Chaitanya Koparkar <ckoparkar@gmail.com>
2022-05-06 08:43:53 -0400

    Revert "Revert Can use mutable arrays with a flag"
    This reverts commit 2a7f0b7de862b010421ffdb54f747ec61298c619.

--- src/Array/Mutable.hs
+++ src/Array/Mutable.hs
@@ -1,7 +1,4 @@
--- We don't have any cyclic module dependencies. This file serves the role of
--- an ML-like signature file for the Array API.
-
-module Array
+module Array.Mutable
   (
     -- * Array type
     Array
@@ -15,9 +12,6 @@ module Array
     -- * Convert to/from lists
   , fromList, toList
 
-    -- * LiqidHaskell lemmas
-  , lma_gs, lma_gns, lma_swap, lma_swap_eql
-
   ) where
 
 --------------------------------------------------------------------------------
@@ -25,21 +19,34 @@ module Array
 data Array a
 
 make :: Int -> a -> Array a
+make = _todo
+
 get :: Array a -> Int -> a
+get = _todo
+
 set :: Array a -> Int -> a -> Array a
+set = _todo
+
 slice :: Array a -> Int -> Int -> Array a
+slice = _todo
+
 size :: Array a -> Int
+size = _todo
+
 append :: Array a -> Array a -> Array a
+append = _todo
+
 swap :: Array a -> Int -> Int -> Array a
+swap = _todo
+
 size2 :: Array a -> (Int, Array a)
+size2 = _todo
+
 get2 :: Array a -> Int -> (a, Array a)
+get2 = _todo
+
 fromList :: [a] -> Array a
+fromList = _todo
+
 toList :: Array a -> [a]
-
-
---------------------------------------------------------------------------------
-
-lma_gs :: Array a -> Int -> a -> Proof
-lma_gns :: Array a -> Int -> Int -> a -> Proof
-lma_swap :: Array a -> Int -> Int -> Proof
-lma_swap_eql :: Array a -> Int -> Int -> Int -> Proof
+toList = _todo
