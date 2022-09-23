4e799578fa3548ebc9b6f80b51bf4831a7e4c264
Chaitanya Koparkar <ckoparkar@gmail.com>
2022-09-23 10:56:41 -0400

    Bring changes from prof into src

--- src/DpsMerge.hs
+++ src/DpsMerge.hs
@@ -146,3 +146,123 @@ merge' !src1 !src2 !dst i1 i2 j =
 merge :: Ord a => A.Array a -> A.Array a -> A.Array a -> (A.Array a, A.Array a)
 merge src1 src2 dst = merge' src1 src2 dst 0 0 0   -- the 0's are relative to the current
                                                    --   slices, not absolute indices
+
+
+--------------------------------------------------------------------------------
+
+{-
+
+goto_seqmerge :: Int
+goto_seqmerge = 4
+
+
+--
+-- Version (1): explicitly set the pivot element
+--
+
+merge_par :: (HasCallStack, Show a, Ord a) => A.Array a -> A.Array a -> A.Array a -> (A.Array a, A.Array a)
+merge_par !src1 !src2 !dst = -- traceShow ("merge_par", A.toList src1, A.toList src2, dst) $
+    if A.size dst < goto_seqmerge
+    then merge src1 src2 dst
+    else let !(n1, src1') = A.size2 src1
+             !(n2, src2') = A.size2 src2
+             -- !(n3, dst') = A.size2 dst
+             dst' = dst
+             in if n1 == 0
+                then let !(src2'1, dst'') = A.copy2 src2' 0 dst' 0 n2
+                     in (A.append src1' src2'1, dst'')
+                else if n2 == 0
+                     then let !(src1'1, dst'') = A.copy2 src1' 0 dst' 0 n1
+                          in (A.append src1'1 src2', dst'')
+                     else let mid1 = n1 `div` 2
+                              pivot = A.get src1' mid1
+                              (mid2, src2'') = binarySearch src2' pivot
+
+                              -- split src1
+                              !(src1_l, src1_r) = A.splitAt mid1 src1'
+                              !(src1_r1, src1_r2) = A.splitAt 1 src1_r
+
+                              -- split src2
+                              !(src2_l, src2_r) = A.splitAt mid2 src2''
+
+                              -- set the pivot
+                              !dst1 = A.set dst' (mid1+mid2) pivot
+
+                              -- split dst
+                              !(dst_l, dst_r) = A.splitAt (mid1+mid2) dst1
+                              !(dst_r1, dst_r2) = A.splitAt 1 dst_r
+
+                              -- sort the sub arrays
+                              !(src_l, dst2) = merge_par src1_l src2_l dst_l
+                              !(src_r, dst3) = merge_par src1_r2 src2_r dst_r2
+
+                              -- append srcs and dsts
+                              src''' = A.append src_l (A.append src1_r1 src_r)
+                              dst''' = A.append dst2 (A.append dst_r1 dst3)
+                              -- src''' = A.append src_l src_r
+                              -- dst''' = A.append dst2 dst3
+
+                          in  -- traceShow (mid1,mid2,pivot,dst1,A.set dst' (mid1+mid2) pivot)
+                             -- trace ("pivot=" ++ show pivot) $
+                              (src''', dst''')
+
+
+--
+-- Version (2): don't explicit set the pivot
+--
+
+merge_par :: (Show a, Ord a) => A.Array a -> A.Array a -> A.Array a -> (A.Array a, A.Array a)
+merge_par !src1 !src2 !dst = traceShow ("merge_par", A.toList src1, A.toList src2, dst) $
+    if A.size dst < goto_seqmerge
+    then merge src1 src2 dst
+    else let !(n1, src1') = A.size2 src1
+             !(n2, src2') = A.size2 src2
+             !(n3, dst')  = A.size2 dst
+             in if n1 == 0
+                then let !(src2'1, dst'') = A.copy2 src2' 0 dst' 0 n2
+                     in (A.append src1' src2'1, dst'')
+                else if n2 == 0
+                     then let !(src1'1, dst'') = A.copy2 src1' 0 dst' 0 n1
+                          in (A.append src1'1 src2', dst'')
+                     else let mid1            = n1 `div` 2
+
+                              (pivot, src1'1) = A.get2 src1' mid1
+                              (mid2,  src2'1) = binarySearch src2' pivot
+
+                              -- (src1_l, src1'2) = A.slice2 src1'1 0 mid1
+                              -- (src1_r, src1'3) = A.slice2 src1'2 mid1 (n1 - mid1 + 1)
+                              -- (src2_l, src2'2) = A.slice2 src2'1 0 mid2
+                              -- (src2_r, src2'3) = A.slice2 src2'2 mid2 (n2 - mid2 + 1)
+
+                              (src1_l, src1_r) = A.splitAt mid1 src1'1
+                              (src2_l, src2_r) = A.splitAt mid2 src2'1
+
+                              -- !dst''           = A.set dst' (mid1+mid2) pivot
+
+                              (dst_l, dst'1)  = A.slice2 dst'  0 (mid1+mid2)
+                              (dst_r, dst'2)  = A.slice2 dst'1 (mid1+mid2) (n3 - (mid1+mid2))
+
+                              !(src_l, dst_l') = merge_par src1_l src2_l dst_l
+                              !(src_r, dst_r') = merge_par src1_r src2_r dst_r
+                              src'''          = A.append src_l  src_r
+                              dst'''          = A.append dst_l' dst_r'
+                           in trace ("pivot=" ++ show pivot ++ ", mid1=" ++ show mid1 ++ ", mid2=" ++ show mid2
+                                     ++ ", arrays=" ++ show (map A.toList [src1_l, src1_r, src2_l, src2_r]) ) $
+                                (src''', dst''')
+
+
+-}
+
+
+binarySearch :: Ord a => A.Array a -> a -> (Int, A.Array a)
+binarySearch ls query = (go 0 (A.size ls), ls)
+  where
+    go lo hi = if n == 0
+               then lo
+               else if query < pivot
+                    then go lo mid
+                    else go (mid+1) hi
+      where
+        n = hi - lo
+        mid = lo + n `div` 2
+        pivot = A.get ls mid
