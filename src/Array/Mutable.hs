31f7467a067804ae25eb4db8d57ebc815c9c7b7d
Chaitanya Koparkar <ckoparkar@gmail.com>
2023-04-10 09:19:24 -0400

    Rejigger mutable array implementation

--- src/Array/Mutable.hs
+++ src/Array/Mutable.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE CPP              #-}
-{-# LANGUAGE UnliftedNewtypes #-}
-{-# LANGUAGE UnboxedTuples    #-}
 {-# LANGUAGE MagicHash        #-}
 {-# LANGUAGE BangPatterns     #-}
 {-# LANGUAGE LinearTypes      #-}
@@ -37,6 +35,7 @@ module Array.Mutable {-
 import qualified Unsafe.Linear as Unsafe
 import           Control.DeepSeq ( NFData(..) )
 import qualified GHC.Exts as GHC
+import           Array.Mutable.Unlifted
 
 --------------------------------------------------------------------------------
 -- Mutable, lifted array API
@@ -125,7 +124,7 @@ splitAt m xs = (slice xs 0 m, slice xs m n)
 append :: Array a -> Array a -> Array a
 append (Array l1 _r1 !a1) (Array _l2 r2 _a2) = Array l1 r2 a1
 
--- token xs == token ys 
+-- token xs == token ys
 -- lem_slice_append :: Array a -> Array a -> ()
 -- lem_slice_append xs ys  = ()
 
@@ -147,62 +146,3 @@ toList :: Array a -> [a]
 toList arr =
   let ixs = [0..(size arr - 1)]
   in [ get arr i | i <- ixs ]
-
-
---------------------------------------------------------------------------------
--- Mutable, unlifted array API
---------------------------------------------------------------------------------
-
--- | A mutable array holding @a@s
-newtype Array# a = Array# (GHC.MutableArray# GHC.RealWorld a)
-
--- The NOINLINE pragmas prevent the runRW# effect from being reordered.
-
-{-# NOINLINE make# #-}
-make# :: GHC.Int# -> a -> Array# a
-make# s a =
-  GHC.runRW# $ \st ->
-    case GHC.newArray# s a st of
-      (# _, arr #) -> Array# arr
-
-
-{-# NOINLINE get# #-}
-get# :: Array# a -> GHC.Int# -> a
-get# (Array# !arr) i =
-  case GHC.runRW# (GHC.readArray# arr i) of
-    (# _, !ret #) -> ret
-
-
-{-# NOINLINE set# #-}
-set# :: Array# a -> GHC.Int# -> a -> Array# a
-set# (Array# !arr) i !a =
-  case GHC.runRW# (GHC.writeArray# arr i a) of
-    _ -> Array# arr
-
-{-# NOINLINE copy# #-}
-copy# :: Array# a -> GHC.Int# -> Array# a -> GHC.Int# -> GHC.Int# -> Array# a
-copy# (Array# !src) src_offset (Array# !dst) dst_offset n =
-  case GHC.runRW# (GHC.copyMutableArray# src src_offset dst dst_offset n) of
-    _ -> Array# dst
-
-size# :: Array# a -> Int
-size# (Array# arr) =
-  let !s = GHC.sizeofMutableArray# arr
-  in GHC.I# s
-
-toList# :: Array# a -> [a]
-toList# arr =
-  let ixs = [0..(size# arr - 1)]
-  in [ get# arr i | (GHC.I# i) <- ixs ]
-
-fromList# :: [a] -> Array# a
-fromList# [] = make# 0# undefined
-fromList# ls =
-  let (GHC.I# len)= (length ls)
-      a0 = make# len (head ls)
-  -- in foldl (\acc (i,x) -> set# acc i x) a0 (zip [0..] ls)
-  in go a0 (zip [0..] ls)
-  where
-    go :: Array# a -> [(Int,a)] -> Array# a
-    go acc []          = acc
-    go acc ((GHC.I# i,x):rst) = go (set# acc i x) rst
