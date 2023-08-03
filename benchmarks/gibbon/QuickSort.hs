3e2ef1edbe87b0747a738560f37022c9fc3427b9
Joseph Zullo <josephazullo@gmail.com>
2023-08-03 14:47:40 -0400

    Update gibbon/QuickSort.hs

--- benchmarks/gibbon/QuickSort.hs
+++ benchmarks/gibbon/QuickSort.hs
@@ -2,34 +2,33 @@ module QuickSort where
 
 import Gibbon.Vector
 
-quickSort :: Vector Int -> Vector Int
+quickSort :: Vector Int -> (Int -> Int -> Int) -> Vector Int
 quickSort xs cmp =
   let len = size xs
-      cpy = alloc len
-  in quickSortBtw cpy 0 len
+  in quickSortBtw xs cmp 0 len
 
-quickSortBtw :: Vector Int -> Int -> Int -> Vector Int
-quickSortBtw xs i j  =
+quickSortBtw :: Vector Int -> (Int -> Int -> Int) -> Int -> Int -> Vector Int
+quickSortBtw xs cmp i j  =
   if j - i < 2
   then xs
-  else let (xs', i_piv) = shuffleBtw xs i j
-           xs''         = quickSortBtw xs'  i           i_piv
-           xs'''        = quickSortBtw xs'' (i_piv + 1) j
+  else let (xs', i_piv) = shuffleBtw xs cmp i j
+           xs''         = quickSortBtw xs' cmp  i           i_piv
+           xs'''        = quickSortBtw xs'' cmp (i_piv + 1) j
         in xs'''
 
-shuffleBtw :: Vector Int -> Int -> Int -> (Vector Int, Int)
-shuffleBtw xs i j =
+shuffleBtw :: Vector Int -> (Int -> Int -> Int) -> Int -> Int -> (Vector Int, Int)
+shuffleBtw xs cmp i j =
   let
       (piv, xs1) = get2 xs (j-1)
       goShuffle zs jl jr = 
         if jl > jr
         then (zs, jl)
         else let (vl, zs') = get2 zs jl in
-          if vl <= piv
+          if cmp vl piv < 1
           then goShuffle zs' (jl+1)
                              jr
           else let (vr, zs'') = get2 zs' jr in
-            if vr >  piv
+            if cmp vr piv > 0
             then goShuffle zs'' jl     (jr-1)
             else let zs''' = swap zs'' jl jr
                   in goShuffle zs''' jl (jr-1)
