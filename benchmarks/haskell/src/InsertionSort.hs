6657da881d29ad68d748f0b9a75fdaf213485cfe
vidsinghal <vidush.sl@gmail.com>
2024-10-07 21:11:02 -0400

    InsertionSort haskell

---
+++ benchmarks/haskell/src/InsertionSort.hs
@@ -0,0 +1,21 @@
+
+insert :: Int -> [Int] -> [Int]
+insert elem lst = case lst of 
+                        [] -> [elem]
+                        x:xs -> if x > elem 
+                                then elem:lst
+                                else x:(insert elem xs)
+
+insertionSort :: [Int] -> [Int]
+insertionSort list = case list of 
+                            [] -> [] 
+                            x:rst -> let 
+                                        sortedRst = insertionSort rst 
+                                        list' = insert x sortedRst
+                                      in list'
+
+main = do 
+    print (insertionSort [3, 2, 1, 2, 10, 33, 22, 34, 2, 0, 1, 2, 2, 3, 4, 98, 89])
+
+
+
