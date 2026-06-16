4568fdff3bf503cfd15b77bda187044c78a1a1b4
Michael Borkowski <mhborkow@purdue.edu>
2026-06-16 18:30:05 -0400

    work towards lowering allocation on the heap

--- src/Array/Mutable.hs
+++ src/Array/Mutable.hs
@@ -19,6 +19,8 @@ in [linear-base](https://github.com/tweag/linear-base).
 -}
 module Array.Mutable where
 
+import Data.Bits (unsafeShiftL, unsafeShiftR, (.&.), (.|.))
+
 import           Linear.Common
 import qualified Unsafe.Linear as Unsafe
 import           Control.DeepSeq ( NFData(..) )
@@ -47,128 +49,147 @@ type HasPrim a =
   Unconstrained a
 #endif
 
-{-@ data Array a = Array { left :: _, right :: _, arr :: _ } @-}
+{-# INLINE pack #-}
+pack :: Int -> Int -> Int -- (lower, size) -> packed
+pack lower size = (lower `unsafeShiftL` 32) .|. (size .&. 0xFFFFFFFF)
 
-data Array a = Array { lower :: {-# UNPACK #-} !Int
-                     , upper :: {-# UNPACK #-} !Int
-                     , array ::                !(Array# a)
+{-# INLINE lowerOf #-}
+lowerOf :: Int -> Int
+lowerOf p = (p `unsafeShiftR` 32) .&. 0xFFFFFFFF
+
+{-# INLINE sizeOf #-}
+sizeOf :: Int -> Int
+sizeOf  p = p .&. 0xFFFFFFFF
+
+{-# INLINE upperOf #-}
+upperOf :: Int -> Int
+upperOf p = lowerOf p + sizeOf p
+
+data Array a = Array { packed :: {-# UNPACK #-} !Int
+                     , array  ::                !(Array# a)
                      }
-
 
 #ifdef PRIM_MUTABLE_ARRAYS
 instance (Show a, P.Prim a) => Show (Array a) where
 #else
 instance Show a => Show (Array a) where
 #endif
-  show (Array l r arr) =
-    "Array { lower = " ++ show l ++ ", upper = " ++ show r ++ ", arr = " ++
+  show (Array p arr) =
+    "Array { lower = " ++ show (lowerOf p) ++ ", size = " ++ show (sizeOf p) ++ ", arr = " ++
     (show $ toList# arr)
 
 instance NFData a => NFData (Array a) where
-  rnf (Array lo hi _arr) = rnf lo `seq` rnf hi `seq` ()
+  rnf (Array p _arr) = rnf p `seq` ()
 
 {-# INLINABLE make #-}
 make :: HasPrim a => Int -> a -> Array a
-make 0 _ = Array 0 0 undefined
-make s0@(GHC.I# s) x = Array 0 s0 (make# s x)
+make 0 _ = Array 0 undefined
+make s0@(GHC.I# s) x = Array (pack 0 s0) (make# s x)
 
 {-# INLINABLE makeNoFill #-}
 makeNoFill :: HasPrim a => Int -> a -> Array a
 makeNoFill s0@(GHC.I# s) x =
-  Array 0 s0
+  Array (pack 0 s0)
 #ifdef PRIM_MUTABLE_ARRAYS
   (makeNoFill# s x)
 #else
   (make# s x)
 #endif
 
-{-# INLINABLE size #-}
+{-# INLINE size #-}
 size :: Array a -> Int
-size (Array !lo !hi _arr) = hi-lo
+size (Array !p _arr) = sizeOf p
 
-{-# INLINABLE get #-}
+{-# INLINE get #-}
 get :: HasPrim a => Array a -> Int -> a
-get (Array lo'@(GHC.I# lo) _hi !arr) i'@(GHC.I# i) =
+get (Array !p !arr) i'@(GHC.I# i) =
   seq
 #ifdef RUNTIME_CHECKS
-  ( if i' < 0 || i' >= (_hi - lo')
-    then (error $ "get: index out of bounds: i = " ++ show i' ++ ", relative to " ++ show lo' ++ "," ++ show _hi)
+  ( if i' < 0 || i' >= (sizeOf p)
+    then (error $ "get: index out of bounds: i = " ++ show i' ++ 
+                    ", relative to " ++ show (lowerOf p) ++ "," ++ show (upperOf p))
     else () )
 #else
   ()
 #endif
-  get# arr (lo GHC.+# i)
+  (let (GHC.I# lo) = lowerOf p in 
+      get# arr (lo GHC.+# i))
 
-{-# INLINABLE set #-}
+{-# INLINE set #-}
 set :: HasPrim a => Array a -> Int -> a -> Array a
-set (Array lo'@(GHC.I# lo) hi !arr) i'@(GHC.I# i) !a =
+set (Array !p !arr) i'@(GHC.I# i) !a =
   seq
 #ifdef RUNTIME_CHECKS
   ( if i' < 0 || i' >= (hi - lo')
-    then (error $ "set: index out of bounds: i = " ++ show i' ++ ", relative to " ++ show lo' ++ "," ++ show hi)
+    then (error $ "set: index out of bounds: i = " ++ show i' ++ 
+                    ", relative to " ++ show (lowerOf p) ++ "," ++ show (upperOf p))
     else () )
 #else
   ()
 #endif
-  Array (GHC.I# lo) hi (set# arr (lo GHC.+# i) a)
+  (let (GHC.I# lo) = lowerOf p in 
+      Array p (set# arr (lo GHC.+# i) a))
 
-{-# INLINABLE copy #-}
+{-# INLINE copy #-}
 copy :: HasPrim a => Array a -> Int -> Array a -> Int -> Int -> Array a
-copy s@(Array (GHC.I# lo1) _hi1 src) (GHC.I# src_offset)
-     d@(Array (GHC.I# lo2) _hi2 dst) (GHC.I# dst_offset)
+copy s@(Array p1 src) (GHC.I# src_offset)
+     d@(Array p2 dst) (GHC.I# dst_offset)
      (GHC.I# n) =
+  let (GHC.I# lo1) = lowerOf p1
+      (GHC.I# lo2) = lowerOf p2 in
 #ifdef PRIM_MUTABLE_ARRAYS
-  case copy# (get# src lo1) src (lo1 GHC.+# src_offset) dst (lo2 GHC.+# dst_offset) n of
-    dst_arr' -> d { array = dst_arr' }
+    case copy# (get# src lo1) src (lo1 GHC.+# src_offset) dst (lo2 GHC.+# dst_offset) n of
+        dst_arr' -> d { array = dst_arr' }
 #else
-  case copy#                src (lo1 GHC.+# src_offset) dst (lo2 GHC.+# dst_offset) n of
-    dst_arr' -> d { array = dst_arr' }
+    case copy#                src (lo1 GHC.+# src_offset) dst (lo2 GHC.+# dst_offset) n of
+        dst_arr' -> d { array = dst_arr' }
 #endif
 
 
-{-# INLINABLE copy2 #-}
+{-# INLINE copy2 #-}
 copy2 :: HasPrim a => Int -> Int -> Int -> (Array a -. (Array a -. (Array a, Array a)))
 copy2 xi yi n = Unsafe.toLinear (\xs -> Unsafe.toLinear (\ys -> (xs, copy xs xi ys yi n)))
 
-{-# INLINABLE slice #-}
+{-# INLINE slice #-}
 slice :: Array a -> Int -> Int -> Array a
-slice (Array l _r !a) l' r' = Array (l+l') (l+r') a
+slice (Array !p !a) l' r' = 
+    Array (pack ((lowerOf p)+l') (r'-l')) a
 
-{-# INLINABLE slice2 #-}
+{-# INLINE slice2 #-}
 slice2 :: Array a -> Int -> Int -> (Array a, Array a)
 slice2 !ar l' r' = (slice ar l' r', ar)
 
-{-# INLINABLE splitAt #-}
+{-# INLINE splitAt #-}
 splitAt :: Int -> (Array a -. (Array a, Array a))
 splitAt m = Unsafe.toLinear (\xs -> (slice xs 0 m, slice xs m (size xs)))
 
-{-# INLINABLE append #-}
+{-# INLINE append #-}
 -- PRE-CONDITION: the two slices are backed by the same array and should be contiguous.
 append :: Array a -. Array a -. Array a
 append xs ys =
   let !res = Unsafe.toLinear (\xs -> case xs of
-        (Array !l1 _r1 !a1) -> Unsafe.toLinear (\ys -> case ys of
-          (Array _l2 !r2 _a2) -> Array l1 r2 a1)) xs ys
+        (Array !p1 !a1) -> Unsafe.toLinear (\ys -> case ys of
+          (Array !p2 _a2) -> Array (pack (lowerOf p1) (sizeOf p1 + sizeOf p2)) a1)) xs ys
   in res
 
 -- token xs == token ys
 -- lem_slice_append :: Array a -> Array a -> ()
 -- lem_slice_append xs ys  = ()
 
-{-# INLINABLE size2 #-}
+{-# INLINE size2 #-}
 size2 :: Array a -. (Ur Int, Array a)
 size2 = Unsafe.toLinear (\ar -> (Ur (size ar), ar))
 
-{-# INLINABLE get2 #-}
+{-# INLINE get2 #-}
 get2 :: HasPrim a => Int -> (Array a -. (Ur a, Array a))
 get2 i = Unsafe.toLinear (\ar -> (Ur (get ar i), ar))
 
-{-# INLINABLE setLin #-}
+{-# INLINE setLin #-}
 setLin :: HasPrim a => Int -> a -> (Array a -. Array a)
 setLin n y = Unsafe.toLinear (\ar -> set ar n y)
 
 fromList :: HasPrim a => [a] -> Array a
-fromList [] = Array 0 0 undefined
+fromList [] = Array 0 undefined
 fromList ls =
   let a0 = make (length ls) (head ls)
   in foldl (\acc (i,x) -> set acc i x) a0 (zip [0..] ls)
